106013238e2adab4abec0caca3d51d21d16f3bdd
st1923 <santiagotoloza@hotmail.com>
2022-11-11 20:26:20 -0300

    terminado

--- index.js
+++ index.js
@@ -1,35 +1,28 @@
 const express = require("express");
-const { restart } = require("nodemon");
 const app = express();
 const ytdl = require("ytdl-core");
 
 app.set("view engine", "ejs");
 
-app.listen(3001, ()=>{
-    console.log('server is running on https://localhost:3001');
-})
+app.get("/", (req, res) => {
+	return res.render("index");
+});
 
-
-app.get("/",(request,res)=>{
-    return  res.render("index.js");
-})
-
-//Creando la ruta de descarga
-app.get('\download' , (req,res) => {
-    //Guardo la id ejemplo "https://www.youtube.com/watch?v=    o85OkeVtm7k "
-    const v_id = req.query.url.split("v=")[1];
-
-})
-
-app.get("/download",async(req,res) => {
-    const v_id = req.query.url.split('v=')[1];
+app.get("/download", async(req, res) => {
+	const v_id = req.query.url.split('v=')[1];
     const info = await ytdl.getInfo(req.query.url);
+    console.log(info.formats[4]);
+    console.log(info.formats[1]);
 
-    return res.render("download",{
-        url: "https://www.youtube.com/embed/" + v_id,
-        info : info.formats.sort((a,b)=>{
+	return res.render("download", {
+		url: "https://www.youtube.com/embed/" + v_id,
+        info: info.formats.sort((a, b) => {
             return a.mimeType < b.mimeType;
+        }),
+	});
+});
 
-        })
-    })
-})
+
+app.listen(3000, () => {
+	console.log("Server is running on http://localhost:3000");
+});
